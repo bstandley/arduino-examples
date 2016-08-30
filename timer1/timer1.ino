b10372593b2ce64a012ee3c0302ec63f85d51659
Brian Standley <brian@brianstandley.com>
2016-08-30 17:41:51 +0200

    Add option to disable test pulses, configurable loop delay

--- timer1/timer1.ino
+++ timer1/timer1.ino
@@ -2,10 +2,12 @@
 
 // notes:
 //  - tested on a Leonardo with a wire connecting pin 8 and 12 together
-//  - 500 reps took about 3500 us, so the counter can read external signals up to at least 140 kHz
-//  - max counting frequency is unclear (without actually consulting the datasheet) -- internal clock runs
-//    at 16MHz but is divided down for use by the counters
-//  - narrow pulses, i.e. sub-us, have not yet been tested
+//  - 500 reps took about 3500 us, so based on this the counter can read self-
+//    generated signals up to at least 140 kHz
+//  - max counting frequency is unclear (without actually consulting the
+//    datasheet) -- internal clock runs at 16MHz but is divided down for use
+//    by the counters
+//  - tested with a frequency generator up to 5 MHz, i.e. pulse width of 0.1us
 //  - overflow-handing code would be needed in a real application . . .
 
 // docs:
@@ -13,9 +15,11 @@
 //  http://maxembedded.com/2011/06/avr-timers-timer1/
 //  http://www.pjrc.com/teensy/td_libs_FreqCount.html
 
-const int clock_input_pin = 12;  // board-dependent, see docs
-const int test_signal_pin = 8;   // arbitrary
-const int test_reps = 500;
+const int clock_input_pin = 12;    // board-dependent, see docs
+const int test_signal_pin = 8;     // arbitrary
+const int test_reps       = 0;     // set to zero to disable
+const int count_delay     = 10;    // ms
+const int loop_delay      = 2000;  // ms
 
 void setup()
 {
@@ -32,24 +36,40 @@ void setup()
 
 void loop()
 {
-    unsigned int count = TCNT1;
+    unsigned int c_start = TCNT1;
 
-    Serial.print("count = ");
-    Serial.println(count);
+    Serial.print("initial count = ");
+    Serial.println(c_start);
 
-    unsigned long t_start = micros();
-    for (int i = 0; i < test_reps; i++)
-    {
-        digitalWrite(test_signal_pin, HIGH);
-        digitalWrite(test_signal_pin, LOW);
-    }
-    unsigned long t_end = micros();
+	if (test_reps > 0)
+	{
+	    unsigned long t_start = micros();
+    	for (int i = 0; i < test_reps; i++)
+	    {
+	        digitalWrite(test_signal_pin, HIGH);
+	        digitalWrite(test_signal_pin, LOW);
+	    }
+	    unsigned long t_end = micros();
 
-    Serial.print("generate ");
-    Serial.print(test_reps);
-    Serial.print(" test pulses in ");
-    Serial.print(t_end - t_start);
-    Serial.println(" us");
+	    Serial.print("generate ");
+	    Serial.print(test_reps);
+	    Serial.print(" test pulses in ");
+	    Serial.print(t_end - t_start);
+	    Serial.println(" us");
+	}
 
-    delay(1000);
+	delay(count_delay);
+    unsigned int c_end = TCNT1;
+
+	Serial.print("wait ");
+	Serial.print(count_delay);
+	Serial.println(" ms");
+
+    Serial.print("final count = ");
+    Serial.print(c_end);
+	Serial.print(" (+");
+	Serial.print(c_end - c_start);
+	Serial.println(")");
+
+    delay(loop_delay);
 }
