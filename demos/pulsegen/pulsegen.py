9affd59c53600dbf223c8607748ae3c79ad3da46
Brian Standley <brian@brianstandley.com>
2016-09-23 17:28:55 +0200

    Add script demonstrating remote control of pulse generator

---
+++ demos/pulsegen/pulsegen.py
@@ -0,0 +1,72 @@
+import serial
+import socket
+
+basevars_full   = ['*IDN',':TRIG:EDGE', ':TRIG:ARMED', ':TRIG:READY', ':TRIG:REARM', ':TRIG:COUNT', ':CLOCK:SRC', ':CLOCK:FREQ', ':CLOCK:FREQ:MEASURE', ':CLOCK:FREQ:INTERNAL', ':CLOCK:FREQ:EXTERNAL']
+basevars_short  = ['*IDN',':TRIG:EDGE', ':TRIG:ARM',   ':TRIG:READY', ':TRIG:REARM', ':TRIG:COUNT', ':CLOCK:SRC', ':CLOCK:FREQ', ':CLOCK:FREQ:MEAS',    ':CLOCK:FREQ:INT',      ':CLOCK:FREQ:EXT']
+pulsevars_full  = [':DELAY', ':WIDTH', ':PERIOD', ':CYCLES', ':VALID', ':INVERT']
+pulsevars_short = [':DEL',   ':WID',   ':PER',    ':CYC',    ':VAL',   ':INV']
+lanvars_full    = [':LAN:MODE', ':LAN:MAC', ':LAN:IP', ':LAN:IP:STATIC', ':LAN:GATEWAY:STATIC', ':LAN:SUBNET:STATIC']
+lanvars_short   = [':LAN:MODE', ':LAN:MAC', ':LAN:IP', ':LAN:IP:STAT',   ':LAN:GATE:STAT',      ':LAN:SUB:STAT']
+
+class Pulsegen :
+
+    def save(self)   : return self.query('*SAV')
+    def recall(self) : return self.query('*RCL')
+    def reset(self)  : return self.query('*RST')
+
+    def dump_base(self, short=False) :
+        for var in (basevars_short if short else basevars_full) :
+            msg = var + '?'
+            print(msg.ljust(25) + self.query(msg).strip())
+
+    def dump_pulse(self, chan=1, short=False) :
+        for var in (pulsevars_short if short else pulsevars_full) :
+            msg = (':PULS' if short else ':PULSE') + str(chan) + var + '?'
+            print(msg.ljust(25) + self.query(msg).strip())
+
+    def dump_lan(self, short=False) :
+        for var in (lanvars_short if short else lanvars_full) :
+            msg = var + '?'
+            print(msg.ljust(25) + self.query(msg).strip())
+
+    def dump_all(self, short=False) :
+        self.dump_base(short=short)
+        for chan in range(1, 5) :
+            self.dump_pulse(chan=chan, short=short)
+        self.dump_lan(short=short)
+
+    def set_pulse(self, chan=1, delay=0.04, width=0.005, period=0.02, cycles=3, invert=0) :
+        msg = ':PULS%d:DEL %f' % (chan, delay)
+        print(msg.ljust(25) + self.query(msg).strip())
+        msg = ':PULS%d:WID %f' % (chan, width)
+        print(msg.ljust(25) + self.query(msg).strip())
+        msg = ':PULS%d:PER %f' % (chan, period)
+        print(msg.ljust(25) + self.query(msg).strip())
+        msg = ':PULS%d:CYC %d' % (chan, cycles)
+        print(msg.ljust(25) + self.query(msg).strip())
+        msg = ':PULS%d:INV %d' % (chan, invert)
+        print(msg.ljust(25) + self.query(msg).strip())
+
+class PulsegenSerial(Pulsegen, serial.Serial) :
+
+    def __init__(self, port, timeout=1) :
+        serial.Serial.__init__(self, port=port, timeout=timeout)
+
+    def query(self, msg) :
+        self.write(msg + '\n')
+        return self.readline()
+
+class PulsegenSocket(Pulsegen, socket.socket) :
+
+    def __init__(self, ip_addr, port=18, timeout=1) :
+        socket.socket.__init__(self, socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((ip_addr, port))
+        self.settimeout(timeout)
+
+    def query(self, msg) :
+        self.sendall(msg + '\n')
+        reply = ''
+        while True :
+            reply += self.recv(512)
+            if reply.endswith('\r') or reply.endswith('\n') : break
+        return reply
